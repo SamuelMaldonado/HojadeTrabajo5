16023035dbfabfeee66e22e8b8f6d99fc4f92364
diaz1202 <alejodiaz.1202@gmail.com>
2014-08-22 08:25:36 -0600

    cambios 2.0

--- hdt5.py
+++ hdt5.py
@@ -1,42 +1,16 @@
 import random
-
 import simpy
 
-randomSeed = 10
-procesos=25
-intervalo=10
-RAM=100
+randomSeed =10
 
-def source(env,number,intervalo,contador):
-    for i in range(number):
-        p = proceso(env,'Proceso%02d' % i, contador,tiempoProceso=1)
-        env.process(p)
-        t = random.expovariate(1.0 / intervalo)
-        yield env.timeout(t)
-
-def proceso(env,name,counter,tiempoProceso):
+def proceso(env,name,CPU,RAM,tiempoDeEspera,tiempoDeEjecucion):
+    yield env.timeout(tiempoDeEspera)
     arrive =env.now
-    print ('%7.4f %s: Proceso' % (arrive,name))
 
-    with counter.request() as req:
-        instrucciones = random.uniform(1,10)
-        results = yield req | env.timeout(instrucciones)
+    print ('%s Llego Procese en %d' % (name,env.now))
 
-        wait = env.now - arrive
+    with RAM.request() as req:
+        yield req
 
-        if req in results:
-            print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
-
-            tib = random.expovariate(1.0/tiempoProceso)
-            yield env.timeout(tib)
-            print('%7.4f %s: Finished' % (env.now, name))
-        else:
-             print('%7.4f %s: ESPERANDO %6.3f' % (env.now, name, wait))
-
-print ('Computadora')
-random.seed(randomSeed)
-env = simpy.Environment()
-
-counter = simpy.Resource(env,capacity=1)
-env.process(source(env,procesos,intervalo,counter))
-env.run()
+    
+        
