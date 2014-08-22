efde36dc91d06002c93d17e6ccdb0ec730ddd997
diaz1202 <alejodiaz.1202@gmail.com>
2014-08-22 07:48:02 -0600

    cambios 1.0

--- hdt5.py
+++ hdt5.py
@@ -4,6 +4,39 @@ import simpy
 
 randomSeed = 10
 procesos=25
-tiempo= random.expovariate(1.0/10)
+intervalo=10
+RAM=100
 
-print  (tiempo)
+def source(env,number,intervalo,contador):
+    for i in range(number):
+        p = proceso(env,'Proceso%02d' % i, contador,tiempoProceso=1)
+        env.process(p)
+        t = random.expovariate(1.0 / intervalo)
+        yield env.timeout(t)
+
+def proceso(env,name,counter,tiempoProceso):
+    arrive =env.now
+    print ('%7.4f %s: Proceso' % (arrive,name))
+
+    with counter.request() as req:
+        instrucciones = random.uniform(1,10)
+        results = yield req | env.timeout(instrucciones)
+
+        wait = env.now - arrive
+
+        if req in results:
+            print('%7.4f %s: Waited %6.3f' % (env.now, name, wait))
+
+            tib = random.expovariate(1.0/tiempoProceso)
+            yield env.timeout(tib)
+            print('%7.4f %s: Finished' % (env.now, name))
+        else:
+             print('%7.4f %s: ESPERANDO %6.3f' % (env.now, name, wait))
+
+print ('Computadora')
+random.seed(randomSeed)
+env = simpy.Environment()
+
+counter = simpy.Resource(env,capacity=1)
+env.process(source(env,procesos,intervalo,counter))
+env.run()
